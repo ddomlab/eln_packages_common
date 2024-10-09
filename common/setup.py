df1cc30d57ca459c396da91f7d38712ea0713d68
12buntu <kylemhollars@gmail.com>
2024-10-09 00:52:54 -0400

    fixed setup.py files

--- common/setup.py
+++ common/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Ddomlab-ELN-Automation-Common",
+    name="ddomlab_eln_packages_common",
     version="0.1.0",
     packages=find_packages(),
     author="Kyle Hollars",
