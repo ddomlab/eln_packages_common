1a77f7cbc4f1363e6d445267eacbc2eb74e702d2
12buntu <kylemhollars@gmail.com>
2024-10-09 00:30:34 -0400

    added setup.py files

---
+++ common/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Ddomlab-ELN-Automation-Common",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Kyle Hollars",
+    author_email="kmhollar@ncsu.edu",
+    python_requires=">=3.11",
+)
