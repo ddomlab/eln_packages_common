ba18540826739acff887c06ae396b87bea4bf8a0
12buntu <kylemhollars@gmail.com>
2025-05-02 10:48:13 -0400

    added get_inventory

---
+++ get_inventory.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import eln_packages_common.resourcemanage
+
+rm = eln_packages_common.resourcemanage.Resource_Manager()
+
+target_columns = [
+    "title", "category", "Mn", "Mw", "CAS", "PDI", "Room", "State", "Opened", "SMILES", "Location",
+    "Quantity", "Received", "BigSMILES", "Full name", "Lot number", "Hazards Link",
+    "Manufacturer", "Pubchem Link", "Container type", "Molecular Weight", "Solvent",
+    "Solvent CAS", "Concentration", "Solvent SMILES", "Purity", "Big SMILES",
+    "Name", "Smile", "Big Smile"
+]
+
+df = rm.get_items_df(size=1000)
+df = df[target_columns]
