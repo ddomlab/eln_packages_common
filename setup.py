ea2bc5d04b0a5f7d14c499fb37ea375d39cbbb68
12buntu <kylemhollars@gmail.com>
2025-05-15 13:58:39 -0400

    restructured to be a better package, this is def gonna break like everything

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="ddomlab_eln_packages_common",
     version="0.1.0",
-    packages=find_packages(),
+    package_dir={"":"src"},
+    packages=find_packages(where="src"),
     author="Kyle Hollars",
     author_email="kmhollar@ncsu.edu",
     python_requires=">=3.11",
